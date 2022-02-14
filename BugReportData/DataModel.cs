e8fe632536da778311d9b4551c2b99683399b426
Jonathan Scazzola <jonathan.scazzola@gmail.com>
2022-02-14 19:08:29 +0100

    Add files via upload

---
+++ BugReportData/DataModel.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugReportData {
+    // Exceptions belong in a different project, to be referenced by all tiers
+    // we put them here as a shortcut, but it is not a good design
+    [Serializable]
+    public class BRException : Exception {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public BRException() { }
+        public BRException(string message) : base(message) { }
+        public BRException(string message, Exception inner) : base(message, inner) { }
+
+        protected BRException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context) { }
+    }
+
+    [Serializable]
+    public class UnauthorizedException : BRException {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public UnauthorizedException() { }
+        public UnauthorizedException(string message) : base(message) { }
+        public UnauthorizedException(string message, Exception inner) : base(message, inner) { }
+
+        protected UnauthorizedException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context) { }
+    }
+
+    [Serializable]
+    public class UnavailableDbException : BRException {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public UnavailableDbException() { }
+        public UnavailableDbException(string message) : base(message) { }
+        public UnavailableDbException(string message, Exception inner) : base(message, inner) { }
+
+        protected UnavailableDbException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context) { }
+    }
+
+    [Serializable]
+    public class UniquenessViolationException : BRException {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public UniquenessViolationException() { }
+        public UniquenessViolationException(string message) : base(message) { }
+        public UniquenessViolationException(string message, Exception inner) : base(message, inner) { }
+
+        protected UniquenessViolationException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context) { }
+    }
+
+    [Serializable]
+    public class UserUnknownException : BRException {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public UserUnknownException() { }
+        public UserUnknownException(string message) : base(message) { }
+        public UserUnknownException(string message, Exception inner) : base(message, inner) { }
+
+        protected UserUnknownException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context) { }
+    }
+    public class BugReportContext:DbContext {
+        public BugReportContext(string connectionString):base(new DbContextOptionsBuilder<BugReportContext>().UseSqlServer(connectionString).Options){}
+        protected override void OnConfiguring(DbContextOptionsBuilder options) {
+            options.LogTo(Console.WriteLine).EnableSensitiveDataLogging();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            var u = modelBuilder.Entity<User>();
+            u.OwnsOne(user => user.Address);
+            var comm = modelBuilder.Entity<Comment>();
+            comm.HasOne(c => c.Author).WithMany(u => u.Comments).OnDelete(DeleteBehavior.ClientCascade);
+            var prod = modelBuilder.Entity<Product>();
+            prod.HasMany(p => p.IncompatibleWith).WithMany("IncompatibleOtherSide").UsingEntity(pp=>pp.ToTable("Incompatibilities"));
+            prod.HasMany(p => p.DependsOn).WithMany(p => p.NeededBy).UsingEntity(pp=>pp.ToTable("Dependencies"));
+        }
+
+        public override int SaveChanges() {
+            try {
+                return base.SaveChanges();
+            }
+            catch (SqlException e) {
+                throw new UnavailableDbException("Unavailable Db", e);
+            }
+            catch (DbUpdateException e) {
+                var sqlException = e.InnerException as SqlException;
+                if (null==sqlException) throw new BRException("Missing information form Db exception", e);
+                switch (sqlException.Number) {
+                    case 2601: throw new UniquenessViolationException("TODO get info from sqlException for informative error message",e);
+                    //all cases for other possible error codes
+                    default:
+                        throw new BRException("Missing information form Db exception", e);
+                }
+            }
+        }
+        public DbSet<User> Users { get; set; }
+    }
+
+    public class Address {
+        public string Town { get; set; }
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Street { get; set; }
+        public int DoorNumber { get; set; }
+    }
+    public enum Status{Open,InProgress,Closed}
+    [Index(nameof(Login),IsUnique = true,Name = "LoginUnique")]
+    [Index(nameof(FiscalCode),IsUnique = true,Name="FiscalCodeUnique")]
+    public class User {
+        public int UserId { get; set; }
+        [MinLength(8)]
+        [MaxLength(32)]
+        public string Login { get; set; }
+        public string Password { get; set; }//hash della password
+        public bool IsAdmin { get; set; }
+        [MinLength(3)]
+        [MaxLength(50)]
+        [Required]//to make using Validator approach feasible
+        public string Name { get; set; }
+        [MinLength(2)]
+        [MaxLength(256)]
+        public string FamilyName { get; set; }
+        public DateTime Birthdate { get; set; }
+        [RegularExpression(@"\w{6}\d{2}\w\d{2}\w\d{3}\w")]
+        [MaxLength(16)]
+        [MinLength(16)]
+        public string FiscalCode { get; set; }
+        [NotMapped]
+        public int Age => DateTime.Now.Year - Birthdate.Year;
+        public Address? Address { get; set; }
+        public List<Report> Reports { get; set; }
+        public List<Comment> Comments { get; set; }
+
+        public User(string login, string password, bool isAdmin,string name, string familyName, DateTime birthdate, string fiscalCode) {
+            Login = login;
+            Password = password;
+            Name = name;
+            FamilyName = familyName;
+            Birthdate = birthdate;
+            FiscalCode = fiscalCode;
+            Reports = new List<Report>();
+            Comments = new List<Comment>();
+        }
+    }
+    [Index(nameof(Code),IsUnique = true,Name="CodeUnique")]
+    public class Product {
+        public int ProductId { get; set; }
+        public int Code { get; set; }
+        public string CommercialName { get; set; }
+        public string InternalReference { get; set; }
+        public string? Description { get; set; }
+        public List<Product> IncompatibleWith { get; set; }
+        public List<Product> DependsOn { get; set; }
+        public List<Product> NeededBy { get; set; }
+        public List<Report> Reports { get; set; }
+        public Product(int code, string commercialName, string internalReference, string? description) {
+            Code = code;
+            CommercialName = commercialName;
+            InternalReference = internalReference;
+            Description = description;
+            IncompatibleWith = new List<Product>();
+            DependsOn = new List<Product>();
+            NeededBy = new List<Product>();
+            Reports = new List<Report>();
+        }
+    }
+
+    public class Report {
+        public int ReportId { get; set; }
+        public Status Status { get; set; }
+        public DateTime CreatedOn { get; set; }
+        [MaxLength(256)]
+        public string ShortDescription { get; set; }
+        public string? Text { get; set; }
+        public User? Author { get; set; }
+        public int UserId { get; set; }
+        public Product? Product { get; set; }
+        public int ProductId { get; set; }
+        public List<Comment> Comments { get; set; }
+        public Report(Status status, DateTime createdOn, string shortDescription, string? text, int userId, int productId) {
+            Status = status;
+            CreatedOn = createdOn;
+            ShortDescription = shortDescription;
+            Text = text;
+            UserId = userId;
+            ProductId = productId;
+            Comments = new List<Comment>();
+        }
+    }
+
+    public class Comment {
+        public int CommentId { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Text { get; set; }
+        public User? Author { get; set; }
+        public int UserId { get; set; }
+        public Report? Report { get; set; }
+        public int ReportId { get; set; }
+        public Comment(DateTime createdOn, string text, int userId, int reportId) {
+            CreatedOn = createdOn;
+            Text = text;
+            UserId = userId;
+            ReportId = reportId;
+        }
+    }
+}
---
+++ BugReportLogic/LogicClasses.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using BugReportData;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace BugReportLogic {
+    public static class BugReportConstants {
+        public const int MinPwLength = 8;
+        public const string OwnerLogin = "BugReportOwner";
+
+        public static string HashPassword(string pw) {
+            //https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
+            //PBKDF2 classe Rfc2898DeriveBytes
+            //PER EVITARE DI FARE TUTTO IO FACCIO UN ORRORE
+            return pw;
+        }
+    }
+    public class Session {
+        public string ConnectionString { get;  }
+        public int UserId { get;  }
+        public bool IsAdmin { get;  }
+        public Session(string connectionString, int userId, bool isAdmin) {
+            ConnectionString = connectionString;
+            UserId = userId;
+            IsAdmin = isAdmin;
+        }
+
+        public void AddUser(string login, string password, bool isAdmin, string name, string familyName,
+            DateTime birthdate, string fiscalCode, Address? address) {
+            if (!IsAdmin) throw new UnauthorizedException("only admin can add users");
+            var user = new User(login, BugReportConstants.HashPassword(password), isAdmin, name, familyName, birthdate,
+                fiscalCode) { Address = address };
+            /*
+            var vc = new ValidationContext(user);
+            try {
+                Validator.ValidateObject(user,vc,true);
+            }
+            catch (ValidationException e) {
+                if (e.ValidationAttribute.GetType() == typeof(MinLengthAttribute)) {
+                    throw new BRException($"{e.ValidationResult.MemberNames.First()} is too short", e);
+                }
+                //all other possibilities
+            }*/
+            //for each property an independent check
+            var vc = new ValidationContext(user){MemberName = nameof(user.Password)};
+            try {
+                Validator.ValidateProperty(password, vc);
+            }
+            catch (ValidationException e) {
+                if (e.ValidationAttribute.GetType() == typeof(MinLengthAttribute)) {
+                    throw new BRException($"{nameof(user.Password)} is too short", e);
+                }
+                //all other possibilities
+            }
+
+            using (var c= new BugReportContext(ConnectionString)) {
+                c.Users.Add(user);
+                c.SaveChanges();
+            }
+        }
+
+        public void UpdateUserName(int userId, string newName) {
+            if (!IsAdmin&&userId!=UserId) throw new UnauthorizedException("only admin can change another user name");
+            using (var c=new BugReportContext(ConnectionString)) {
+                // TODO enclose in a try-catch to deal with not available DB or non-existing user
+                var user = c.Users.Single(u => u.UserId == userId);
+                var vc = new ValidationContext(user) { MemberName = nameof(user.Name) };
+                Validator.ValidateProperty(newName,vc);//TODO enclose in try-catch to deal with inappropriate new names
+                c.SaveChanges();
+            }
+        }
+
+        public void UpdateUserName(string newName) {
+            UpdateUserName(UserId,newName);
+        }
+
+        public void DeleteUser(int userId) {
+            if (!IsAdmin && userId != UserId) throw new UnauthorizedException("only admin can delete another user");
+            using (var c=new BugReportContext(ConnectionString)) {
+                var user = c.Users.Single(u => u.UserId == userId);//TODO deal with exceptions
+                c.Users.Remove(user);
+                c.SaveChanges();
+            }
+        }
+    }
+
+        public class BugReportSystem {
+        public string ConnectionString { get; }
+        public BugReportSystem(string connectionString) {
+            ConnectionString = connectionString;
+        }
+
+        public Session Login(string login, string pw) {
+            //TODO verify parameters
+            using (var c = new BugReportContext(ConnectionString)) {
+                try {
+                    var owner = c.Users.Single(u =>
+                        u.Login == login && u.Password == BugReportConstants.HashPassword(pw));
+                    return new Session(ConnectionString, owner.UserId,owner.IsAdmin);
+                }
+                catch (SqlException e) {
+                    throw new UnavailableDbException("Unavailable Db", e);
+                }
+                catch (DbUpdateException e) {
+                    //TODO verify that codes etc confirm my guess
+                    throw new UserUnknownException("no user with these credentials", e);
+                }
+            }
+        }
+    }
+
+    public static class BugReportFactory {
+        static void ParameterVerify(string connectionString, string connStringName,string adminPassword, string pwName) {
+            if (String.IsNullOrEmpty(connectionString))
+                throw new ArgumentException("connection strings cannot be null or empty", connStringName);
+            if (String.IsNullOrEmpty(adminPassword))
+                throw new ArgumentException("passwords cannot be empty or null", pwName);
+            if (BugReportConstants.MinPwLength>adminPassword.Length)
+                throw new ArgumentException($"password must be at least {BugReportConstants.MinPwLength} long", pwName);
+        }
+
+
+        public static BugReportSystem InitializeBugTracking(string connectionString, string adminPassword) {
+            ParameterVerify(connectionString, nameof(connectionString), adminPassword, nameof(adminPassword));
+            using (var c= new BugReportContext(connectionString)) {
+                c.Database.EnsureDeleted();
+                c.Database.EnsureCreated();
+                var owner = new User(BugReportConstants.OwnerLogin, BugReportConstants.HashPassword(adminPassword), true,"NAME", "FAMILYNAME",
+                    DateTime.Now, "XYWXYW00Z99W000H");
+                c.SaveChanges();
+            }
+            return new BugReportSystem(connectionString);
+        }
+
+        public static BugReportSystem LoadBugTracking(string connectionString, string adminPassword) {
+            ParameterVerify(connectionString, nameof(connectionString), adminPassword, nameof(adminPassword));
+            using (var c= new BugReportContext(connectionString)) {
+                try {
+                    var owner = c.Users.Single(u =>
+                        u.Login == BugReportConstants.OwnerLogin && u.Password == BugReportConstants.HashPassword(adminPassword));
+                }
+                catch (SqlException e) {
+                    throw new UnavailableDbException("Unavailable Db", e);
+                }
+                catch (DbUpdateException e) {
+                    //TODO verify that codes etc confirm my guess
+                    throw new UserUnknownException("no admin with that password",e);
+                }
+                return new BugReportSystem(connectionString);
+            }
+        }
+    }
+}
